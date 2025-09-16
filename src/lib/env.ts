c949b27c084ddc0f6b1124dbf99098e065b419e7
Krishna <krishna13248@gmail.com>
2025-09-17 00:47:28 +0530

    V0:17Sept25 - changed production url of app in env.ts

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,6 @@
 export function getAppUrl() {
   if (process.env.NODE_ENV === "production") {
-    return "http://localhost:3000";
+    return "https://kam-ui.vercel.app";
   }
 
   return "http://localhost:3000";
